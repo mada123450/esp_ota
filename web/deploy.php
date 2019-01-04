da48bace14426548f5628bbf64a487936dc5375d
mada123450 <46359947+mada123450@users.noreply.github.com>
2019-01-04 06:57:17 +0100

    Update deploy.php
    create deploy directory if not exists

--- web/deploy.php
+++ web/deploy.php
@@ -1,4 +1,8 @@
 <?php
+if(!is_dir("./projects/".dirname($_POST['fileName'])."/deploy")
+{
+	mkdir("./projects/".dirname($_POST['fileName'])."/deploy");        
+}
 copy("./projects/".dirname($_POST['fileName'])."/build/".dirname($_POST['fileName']).".ino.bin", 
      "./projects/".dirname($_POST['fileName'])."/deploy/".dirname($_POST['fileName']).".ino.bin");
 ?>
