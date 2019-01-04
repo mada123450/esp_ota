063fc5e53bea866f8e6866a9feeb06d1a09c12f1
mada123450 <mada123456@gmail.com>
2019-01-04 17:28:23 +0100

    added possibility to import/export all projects to .zip file

---
+++ web/exportProjects.php
@@ -0,0 +1,20 @@
+<?php
+shell_exec("zip -r /tmp/projects.zip projects");
+
+// set example variables
+$filename = "projects.zip";
+$filepath = "/tmp/";
+
+// http headers for zip downloads
+header("Pragma: public");
+header("Expires: 0");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Cache-Control: public");
+header("Content-Description: File Transfer");
+header("Content-type: application/octet-stream");
+header("Content-Disposition: attachment; filename=\"".$filename."\"");
+header("Content-Transfer-Encoding: binary");
+header("Content-Length: ".filesize($filepath.$filename));
+ob_end_flush();
+@readfile($filepath.$filename);
+?>
---
+++ web/importProjects.php
@@ -0,0 +1,53 @@
+<?php
+if(!isset($_POST["submit"]))
+{
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="#" method="post" enctype="multipart/form-data">
+    Select projects.zip upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Projects" name="submit">
+</form>
+
+</body>
+</html>
+<?php
+}
+else
+{
+    $target_dir = "/tmp/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    if($fileType != "zip")
+    {
+        echo "Only .zip archives allowed!";   
+    }
+
+    if(file_exists($target_file))
+        unlink($target_file);
+
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+    {
+        shell_exec("unzip -o \"".$target_file."\" -d /var/www/html");
+?>
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <h1>OK</h1>
+                <script>
+                    close();
+                </script>
+            </body>
+        </html>
+<?php
+        unlink($target_file);
+    }
+    else
+    {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
