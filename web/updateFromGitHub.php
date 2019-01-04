ae599d58b588a106eb369ba2b11ba215334678bf
mada123450 <mada123456@gmail.com>
2019-01-04 18:00:12 +0100

    Update web interface source from github

---
+++ web/updateFromGitHub.php
@@ -0,0 +1,7 @@
+<?php
+echo shell_exec("git clone https://github.com/mada123450/esp_ota.git/ /tmp/web");
+echo shell_exec("rm -r /tmp/web/web/.vs");
+echo shell_exec("rm -r /tmp/web/web/projects");
+echo shell_exec("cp -r -v /tmp/web/web /var/www/html");
+echo shell_exec("rm -r /tmp/web");
+?>
