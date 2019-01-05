a0bfd58658e8f6f25cfd6cbc942220ff14b94a7a
mada123450 <mada123456@gmail.com>
2019-01-05 11:07:07 +0100

    updating default project

--- web/projects/default/default.ino
+++ web/projects/default/default.ino
@@ -14,8 +14,6 @@
   
 #define SERIAL_BAUD				115200
 
-#define MQTT_SERVER				"******"
-  
 #define COMPILE_DATE			__DATE__ " " __TIME__
     
 unsigned long lastCheck = 0;
@@ -29,6 +27,8 @@ void setup()
 	Serial.begin(SERIAL_BAUD);
 	Serial.println(OTA_PROJECT_NAME " Build Date: " COMPILE_DATE);
 	SetupWifi();
+
+    // put your setup code here, to run once:
 }
 
 void SetupWifi() 
@@ -70,19 +70,19 @@ void OTAHandler()
 
 void CheckForUpdate()
 {
-    //Serial.println("[update] checking for update");
+    Serial.println("[update] checking for update");
     t_httpUpdate_return ret = ESPhttpUpdate.update(OTA_HOST_IP, OTA_HOST_PORT, "/getBin.php", OTA_BIN_FILE " ## " COMPILE_DATE);
     switch(ret)
     {
         case HTTP_UPDATE_FAILED:
-            //Serial.println("[update] Update failed.");
+            Serial.println("[update] Update failed.");
              Serial.println(ESPhttpUpdate.getLastErrorString().c_str());
             break;
         case HTTP_UPDATE_NO_UPDATES:
-            //Serial.println("[update] Update no Update.");
+            Serial.println("[update] Update no Update.");
             break;
         case HTTP_UPDATE_OK:
-            //Serial.println("[update] Update ok."); // may not called we reboot the ESP
+            Serial.println("[update] Update ok."); // may not called we reboot the ESP
             break;
     }
 }
